07696c8c6e7482446a823c75a18ad7b357b3ff30
Essam Joubori <essam.joubori@gmail.com>
2014-03-13 20:25:58 -0500

    Added a detroy and edit links with test

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,19 @@
 class TodoItemsController < ApplicationController
+
+  before_action :find_todo_list
+
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
+  	
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
+  	
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
+  	
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
     	flash[:sucess] = "Added todo list item."
@@ -21,7 +25,45 @@ class TodoItemsController < ApplicationController
     end
   end
 
+  def edit
+    
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    
+  end
+
+  def update
+    
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_item_params)
+      flash[:sucess] = "Saved todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "That todo item could not be saved"
+      render action: :edit
+
+    end
+  end
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:sucess]= "Todo list item was deleted."
+    else
+      flash[:error] = "Todo list item can not be deleted."
+  end
+  redirect_to todo_list_todo_items_path
+end
+
+  def url_options
+    {todo_list_id: params[:todo_list_id]}.merge(super)
+    
+  end
+
   private
+
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])    
+  end
 
   def todo_item_params
   	params[:todo_item].permit(:content)
